160e1c3e8fb6ae735cebe792fdcb10f2e7d272e5
samuelsih <samuel.205025@mhs.its.ac.id>
2023-12-28 00:17:28 +0700

    feat: safety mechanism

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Catch N+1 Query
+        Model::preventLazyLoading(! app()->isProduction());
+
+        // Throw exception on missing attr instead of null
+        Model::preventsAccessingMissingAttributes();
     }
 }
